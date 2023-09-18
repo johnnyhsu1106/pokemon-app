1e65407da6ad1c9e53c3483bfc7fecc3185c17ac
Johnny Hsu <yuwei.hsu@dowjones.com>
2023-09-18 17:36:32 -0500

    Finished Apex Interview Challenge without code refactor for optimization

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import './App.css'
 import {
   Box,
@@ -14,8 +14,54 @@ import {
   Wrap,
 } from '@chakra-ui/react';
 
+const MAX_CAPTURED_POKEMONS_NUM = 6;
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [pokemon, setPokemon] = useState(null);
+  const [pokemons, setPokemons] = useState([]);
+  const inputRef = useRef('');
+  const isCaptureButtonDisabled = pokemons.length >= MAX_CAPTURED_POKEMONS_NUM;
+
+  const fetchPokemons = async (query) => {
+    try {
+      const req = await fetch(`https://pokeapi.co/api/v2/pokemon/${query}`);  
+      const data = await req.json();
+      setPokemon(data);
+      
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+
+  const handleCaptureButtonClick = () => {
+    if (pokemons.length > MAX_CAPTURED_POKEMONS_NUM) {
+      return;
+    }
+    setPokemons((prevPokemons) => {
+      return [...prevPokemons, pokemon];
+    });
+  };
+
+
+  const handleInputSearchClick = () => {
+    const query = inputRef.current.value;
+  
+    if (!query || query.trim() === '') {
+      return;
+    }
+
+    fetchPokemons(query);
+    inputRef.current.value = '';
+  };
+  
+  const handlePokemonRemove = (targetIndex) => {
+    setPokemons((prevPokemons) => {
+      return prevPokemons.filter((_, index) => {
+        return index !== targetIndex;
+      })
+    });
+  };
 
   return (
     <Center h='100vh'>
@@ -34,54 +80,64 @@ function App() {
                 borderColor='red.800'
                 bg='white'
                 placeholder='name/id'
+                ref={inputRef}
               />
-              <Button borderWidth={4} borderColor='red.800'>
+              <Button
+                onClick={handleInputSearchClick} 
+                borderWidth={4} borderColor='red.800'>
                 Search
               </Button>
             </HStack>
             <Stack direction='row' spacing={4}>
               <Box>
                 <Box bg='gray.100' rounded='lg'>
-                  <Image src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png' />
+                  <Image src={pokemon?.sprites?.front_default}/>
                 </Box>
               </Box>
               <Stack>
                 <Text color='white' fontSize='xl' fontWeight={600}>
-                  Bulbasaur
+                  {pokemon?.name}
                 </Text>
                 <Text color='white' fontSize='md' fontWeight={600}>
-                  #1
+                  #{pokemon?.order}
                 </Text>
                 <Text color='white'>Type</Text>
                 <Box>
                   <Wrap>
                     <WrapItem>
-                      <Badge borderWidth={3} rounded='md' borderColor='red.800'>
-                        Grass
-                      </Badge>
+                      {pokemon?.types?.map((item, i) => {  
+                        return (
+                        <Badge key={i} borderWidth={3} rounded='md' borderColor='red.800'>
+                          {item?.type?.name}
+                        </Badge>
+                      )
+                      })}
                     </WrapItem>
                   </Wrap>
                 </Box>
                 <HStack color='white'>
                   <Stack>
                     <Text>HP</Text>
-                    <Text>20</Text>
+                    <Text>{pokemon?.stats[0]?.base_stat}</Text>
                   </Stack>
                   <Stack>
                     <Text>Attack</Text>
-                    <Text>35</Text>
+                    <Text>{pokemon?.stats[1]?.base_stat}</Text>
                   </Stack>
                   <Stack>
                     <Text>Defense</Text>
-                    <Text>35</Text>
+                    <Text>{pokemon?.stats[2]?.base_stat}</Text>
                   </Stack>
                   <Stack>
                     <Text>Speed</Text>
-                    <Text>35</Text>
+                    <Text>{pokemon?.stats[3]?.base_stat}</Text>
                   </Stack>
                 </HStack>
                 <Box>
-                  <Button w='full' borderWidth={4} borderColor='red.800'>
+                  <Button
+                    isDisabled={isCaptureButtonDisabled}
+                    onClick={handleCaptureButtonClick} 
+                    w='full' borderWidth={4} borderColor='red.800'>
                     Capture
                   </Button>
                 </Box>
@@ -99,7 +155,14 @@ function App() {
             left='-4px'
           >
             <Box bg='gray.100' rounded='lg'>
-              <Image src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png' />
+              { pokemons.map((pokemon, i) => {
+                return (
+                  <Image
+                    onClick={() => { handlePokemonRemove(i)}} 
+                    key={i} 
+                    src={pokemon?.sprites?.front_default}/>  
+                )}
+              )}
             </Box>
           </Stack>
         </Stack>
